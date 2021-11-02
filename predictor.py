dfa0e8454c85e24a10a36c5c1bb4bfdff1ee290b
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2021-11-03 01:20:40 +0530

    Fake model predictor done

---
+++ predictor.py
@@ -0,0 +1,29 @@
+import sys
+import torch
+from model import Model
+from data import PreProcessor
+import joblib
+
+
+text = sys.argv[1]
+
+preproc = PreProcessor()
+text = preproc.forward(text)
+
+vectorizer = joblib.load('tfidf_for_fakenews.pkl')
+text = vectorizer.transform([text])
+
+print(text.toarray())
+
+text = torch.Tensor(text.toarray())
+
+model = Model()
+model.load_state_dict(torch.load('fake_model_state_dict.pt'))
+model.eval()
+
+y_pred = []
+
+for i, data in enumerate(text):
+    y_pred.append(int(model(data)>0.5))
+
+print(y_pred)
