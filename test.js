9623ffd7c3a7b897bbf79ea3a27e4a81ffb70442
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2021-11-03 12:55:47 +0530

    sample for running python from js

---
+++ test.js
@@ -0,0 +1,7 @@
+const spawn = require('child_process').spawn;
+let text = 'New species of alien have been found on planet Cheemsland. The human race will have a hard time naming the new species as they have only one testicle.';
+const childPython = spawn('python3', ['predictor.py', text]);
+
+childPython.stdout.on('data', (data) => {
+	console.log('Output from Python:', data.toString());
+});
