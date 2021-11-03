6726d66611d3272d34e225153751859a54bed3d6
Vishesh Kumar Singh <47525494+visheshks04@users.noreply.github.com>
2021-11-03 23:48:45 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const spawn = require('child_process').spawn;
-let text = 'New species of alien have been found on planet Cheemsland. The human race will have a hard time naming the new species as they have only one testicle.';
-const childPython = spawn('python3', ['test.py', text]);
-
-childPython.stdout.on('data', (data) => {
-	console.log('Output from Python:', data.toString());
-});
